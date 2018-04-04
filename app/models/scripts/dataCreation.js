e11965c434b28105561b6b9a3ca91806ce10af9a
César Richard <cesar.richard@hds.utc.fr>
2018-04-04 16:49:51 +0200

    travis + package conf update
    Signed-off-by: César Richard <cesar.richard@hds.utc.fr>

--- app/models/scripts/dataCreation.js
+++ app/models/scripts/dataCreation.js
@@ -1,13 +1,26 @@
 exports.execute = db => {
   const data = [];
-  data.push(db.models.role.create({ name: 'Master', canEdit: true, canView: true }));
-  data.push(db.models.role.create({ name: 'Member', canEdit: false, canView: true }));
+
+  // Associations
+  data.push(db.models.association.create({ name: 'PicsArt', login: 'picsart', active: true }));
+  data.push(db.models.association.create({ name: 'SiMDE', login: 'simde', active: true }));
+
+  // Users
   data.push(
     db.models.user.create({
       first_name: 'd_Cesar',
       last_name: 'd_Richard',
       username: 'cerichar',
       mail: 'email1@gmail.com'
+    })
+  );
+
+  data.push(
+    db.models.user.create({
+      first_name: 'd_Celine',
+      last_name: 'd_Boucher',
+      username: 'cboucher',
+      mail: 'email2@gmail.com'
     })
   );
   return Promise.all(data);
