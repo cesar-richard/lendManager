07789faa1329b02762f4c2bfd434b1fbed3fe7e9
César Richard <cesar.richard@hds.utc.fr>
2018-04-04 16:20:39 +0200

    travis + package conf update
    Signed-off-by: César Richard <cesar.richard@hds.utc.fr>

--- migrations/config.js
+++ migrations/config.js
@@ -2,10 +2,6 @@ const config = require('./../config');
 
 module.exports = {
   [config.environment]: Object.assign({}, config.common.database, {
-    host: '127.0.0.1',
-    port: 3306,
-    name: 'picsart',
-    username: 'root',
-    password: ''
+    dialect: 'mysql'
   })
 };
