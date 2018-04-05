2ca7f55b6217aeea78ad380a0714833fee9c1032
César Richard <cesar.richard@hds.utc.fr>
2018-04-05 18:11:30 +0200

    Unit test promises order bugfix
    Signed-off-by: César Richard <cesar.richard@hds.utc.fr>

--- app/models/scripts/dataCreation.js
+++ app/models/scripts/dataCreation.js
@@ -1,40 +1,163 @@
 exports.execute = db => {
   const data = [];
+  data.push(
+    db.models.association
+      .create({
+        name: 'PicsArt',
+        login: 'picsart',
+        active: true
+      })
+      .then(asso => {
+        const categoryBoitiers = db.models.category
+          .create({
+            name: 'Boitiers',
+            associationId: asso.id
+          })
+          .then(category => {
+            db.models.article
+              .create({
+                brand: 'Nikon',
+                model: 'D3300',
+                price: 400,
+                active: true,
+                categoryId: category.id
+              })
+              .then(() => {
+                db.models.article.create({
+                  brand: 'Canon',
+                  model: '1000D',
+                  price: 250,
+                  active: true,
+                  categoryId: category.id
+                });
+              });
+          })
+          .then(() => {
+            db.models.category
+              .create({
+                name: 'Objectifs',
+                associationId: asso.id
+              })
+              .then(category => {
+                db.models.article
+                  .create({
+                    brand: 'Nikon',
+                    model: '35mm f1.8',
+                    price: 150,
+                    active: true,
+                    categoryId: category.id
+                  })
+                  .then(() => {
+                    db.models.article.create({
+                      brand: 'Tokina',
+                      model: '18mm f5.6',
+                      price: 80,
+                      active: true,
+                      categoryId: category.id
+                    });
+                  });
+              });
+          });
+      })
+  );
 
-  // Associations
   data.push(
-    db.models.association.create({ name: 'PicsArt', login: 'picsart', active: true }).then(asso => {
-      const categoryBoitiers = db.models.category.create({ name: 'Boitiers', associationId: asso.id });
-      const categoryObjectifs = db.models.category.create({ name: 'Objectifs', associationId: asso.id });
-    })
-  );
-  data.push(
-    db.models.association.create({ name: 'SiMDE', login: 'simde', active: true }).then(asso => {
-      const categoryPayutcphones = db.models.category.create({
-        name: 'Payutc Phones',
-        associationId: asso.id
-      });
-      const categoryRouters = db.models.category.create({ name: 'Routers', associationId: asso.id });
-    })
+    db.models.association
+      .create({
+        name: 'SiMDE',
+        login: 'simde',
+        active: true
+      })
+      .then(asso => {
+        db.models.category
+          .create({
+            name: 'Payutc Phones',
+            associationId: asso.id
+          })
+          .then(category => {
+            db.models.article
+              .create({
+                brand: 'Payutc Phone',
+                model: 'BDE 1',
+                price: 200,
+                active: true,
+                categoryId: category.id
+              })
+              .then(() => {
+                db.models.article.create({
+                  brand: 'Payutc Phone',
+                  model: 'BDE 2',
+                  price: 200,
+                  active: true,
+                  categoryId: category.id
+                });
+              })
+              .then(() => {
+                db.models.article
+                  .create({
+                    brand: 'Payutc Phone',
+                    model: 'BDE 3',
+                    price: 200,
+                    active: true,
+                    categoryId: category.id
+                  })
+                  .then(() => {
+                    db.models.article.create({
+                      brand: 'Payutc Phone',
+                      model: 'BDE 4',
+                      price: 200,
+                      active: true,
+                      categoryId: category.id
+                    });
+                  });
+              });
+          })
+          .then(() => {
+            db.models.category
+              .create({
+                name: 'Routers',
+                associationId: asso.id
+              })
+              .then(category => {
+                db.models.article
+                  .create({
+                    brand: 'Cisco',
+                    model: '2950 48ports',
+                    price: 400,
+                    active: true,
+                    categoryId: category.id
+                  })
+                  .then(() =>
+                    db.models.article.create({
+                      brand: 'Netgear',
+                      model: 'Switch 24 ports',
+                      price: 200,
+                      active: true,
+                      categoryId: category.id
+                    })
+                  );
+              });
+          });
+      })
   );
 
-  // Users
   data.push(
-    db.models.user.create({
-      first_name: 'd_Cesar',
-      last_name: 'd_Richard',
-      username: 'cerichar',
-      mail: 'email1@gmail.com'
-    })
+    db.models.user
+      .create({
+        first_name: 'd_Cesar',
+        last_name: 'd_Richard',
+        username: 'cerichar',
+        mail: 'email1@gmail.com'
+      })
+      .then(() => {
+        db.models.user.create({
+          first_name: 'd_Celine',
+          last_name: 'd_Boucher',
+          username: 'cboucher',
+          mail: 'email2@gmail.com'
+        });
+      })
   );
 
-  data.push(
-    db.models.user.create({
-      first_name: 'd_Celine',
-      last_name: 'd_Boucher',
-      username: 'cboucher',
-      mail: 'email2@gmail.com'
-    })
-  );
   return Promise.all(data);
 };
--- test/app.js
+++ test/app.js
@@ -11,13 +11,13 @@ const fs = require('fs'),
 
 chai.use(chaiHttp);
 
-const db = new Sequelize(orm.DB_URL, { logging: false });
+const db = new Sequelize(orm.DB_URL, { logging: true });
 
 beforeEach('drop tables, re-create them and populate sample data', done => {
   models.define(db);
   db
     .query('SET FOREIGN_KEY_CHECKS = 0', { raw: true })
-    .then(db.drop())
+    .then(() => db.drop())
     .then(() => db.sync({ force: true }))
     .then(() => dataCreation.execute(db))
     .then(() => db.query('SET FOREIGN_KEY_CHECKS = 1', { raw: true }))
@@ -27,7 +27,6 @@ beforeEach('drop tables, re-create them and populate sample data', done => {
     });
 });
 
-// including all test files
 const normalizedPath = path.join(__dirname, '.');
 fs.readdirSync(normalizedPath).forEach(file => {
   require(`./${file}`);
