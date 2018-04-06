e0ca6c5d8d2aaf6de191e17b082aa0d7b760e9a1
César Richard <cesar.richard@hds.utc.fr>
2018-04-06 10:08:30 +0200

    test
    Signed-off-by: César Richard <cesar.richard@hds.utc.fr>

--- test/app.js
+++ test/app.js
@@ -13,18 +13,19 @@ chai.use(chaiHttp);
 
 const db = new Sequelize(orm.DB_URL, { logging: true });
 
-beforeEach('drop tables, re-create them and populate sample data', done => {
+before('drop tables, re-create them and populate sample data', done => {
   models.define(db);
   db
-    .query('SET FOREIGN_KEY_CHECKS = 0', { raw: true })
+    .query('SET FOREIGN_KEY_CHECKS = 0;', { raw: true })
     .then(() => db.drop())
     .then(() => db.sync({ force: true }))
     .then(() => dataCreation.execute(db))
-    .then(() => db.query('SET FOREIGN_KEY_CHECKS = 1', { raw: true }))
+    .then(() => db.query('SET FOREIGN_KEY_CHECKS = 1;', { raw: true }))
     .then(() => {
       exports.models = db.models;
       done();
-    });
+    })
+    .catch(err => console.log(err));
 });
 
 const normalizedPath = path.join(__dirname, '.');
